f416753971a569f54ecae009b77afc5d4364f66a
nodecode <info@nodecode.de>
2014-04-06 22:41:57 +0200

    release

---
+++ options-sidebar.php
@@ -0,0 +1,100 @@
+<!--<iframe src="http://omninoggin.com/res/sidebar.php?name=<?php echo $this->name_dashed ?>" border=0 width=250 height=950></iframe>-->
+<style>
+#DIV_1 {
+    float: left;
+    height: 293px;
+    width: 222px;
+    font: normal normal normal 12px/normal 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 10px;
+}/*#DIV_1*/
+
+#DIV_2 {
+    float: left;
+    height: 111px;
+    width: 200px;
+    background: rgb(246, 246, 246) none repeat scroll 0% 0% / auto padding-box border-box;
+    border: 1px solid rgb(204, 204, 204);
+    font: normal normal normal 12px/normal 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 0px 0px 10px;
+    padding: 10px;
+}/*#DIV_2*/
+
+#H3_3, #H3_11 {
+    height: 18px;
+    width: 200px;
+    font: normal normal bold 16px/normal 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 0px 0px 10px;
+}/*#H3_3, #H3_11*/
+
+#UL_4 {
+    height: 58px;
+    width: 200px;
+    font: normal normal normal 12px/normal 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 0px;
+    padding: 0px;
+}/*#UL_4*/
+
+#LI_5, #LI_7, #LI_9 {
+    height: 16px;
+    width: 180px;
+    font: normal normal normal 11px/16px 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 5px 0px 5px 20px;
+}/*#LI_5, #LI_7, #LI_9*/
+
+#A_6, #A_8, #A_14, #A_16 {
+    text-align: left;
+    font: normal normal normal 11px/16px 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+}/*#A_6, #A_8, #A_14, #A_16*/
+
+#DIV_10 {
+    float: left;
+    height: 118px;
+    width: 200px;
+    background: rgb(246, 246, 246) none repeat scroll 0% 0% / auto padding-box border-box;
+    border: 1px solid rgb(204, 204, 204);
+    font: normal normal normal 12px/normal 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 0px 0px 10px;
+    padding: 10px;
+}/*#DIV_10*/
+
+#UL_12 {
+    height: 85px;
+    width: 200px;
+    font: normal normal normal 12px/normal 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 0px;
+    padding: 0px;
+}/*#UL_12*/
+
+#LI_13 {
+    height: 32px;
+    width: 180px;
+    font: normal normal normal 11px/16px 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 5px 0px 5px 20px;
+}/*#LI_13*/
+
+#LI_15 {
+    height: 48px;
+    width: 180px;
+    font: normal normal normal 11px/16px 'Lucida Grande', Verdana, Arial, 'Bitstream Vera Sans', sans-serif;
+    margin: 5px 0px 5px 20px;
+}/*#LI_15*/
+</style>
+
+<div class="omni_admin_sidebar" id="DIV_1">
+  <div class="omni_admin_sidebar_section" id="DIV_2">
+    <h3 id="H3_3">Like this plugin?</h3>
+    Why not do any of the following?
+    <ul id="UL_4">
+      <li id="LI_5"><a href="http://wordpress.org/extend/plugins/wp-minify-fix/" target="_blank" id="A_6">Link to it</a></li>
+      <li id="LI_7"><a href="http://wordpress.org/support/view/plugin-reviews/wp-minify-fix#postform" target="_blank" id="A_8">Give it 5-stars on WordPress.org</a></li>
+      <li id="LI_9"><a href="https://github.com/nodecode/wp-minify-fix" target="_blank">Give a star at GitHub</a></li>
+    </ul>
+  </div>
+  <div class="omni_admin_sidebar_section" id="DIV_10">
+  <h3 id="H3_11">Having Issues?</h3>
+  <ul id="UL_12">
+    <li id="LI_13">Read all documentation on the <a href="http://omninoggin.com/wordpress-plugins/wp-minify-wordpress-plugin/#documentation" target="_blank" id="A_14">plugin homepage (unfixed)</a>.</li>
+    <li id="LI_15">If you still couldn't find your a solution, try searching the <a href="http://wordpress.org/support/" target="_blank" id="A_16">WordPress support forum</a>.</li>
+  </ul>
+  </div>
+</div>
